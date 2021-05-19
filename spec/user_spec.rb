13e0f6ab76cad5c86a13a867c0a5dd617d6e136a
Henry-Cantrell <hencan.1997@gmail.com>
2021-05-18 22:27:48 -0400

    Added user test for likes

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -33,8 +33,16 @@ RSpec.describe User, :type => :model do
     it "can make comments on posts" do
         user = User.create(id:1, email:'test@test.com', password:'test123')
         user.created_posts.create(id: 1, title:'test', body:'test', creator_id:user.id)
-        
+
         expect(user.post_comments.create(post_id:1, author_id:1)).to be_truthy
+    end
+
+    it "can make likes on posts" do
+        user = User.create(id:1, email:'test@test.com', password:'test123')
+        user.created_posts.create(id: 1, title:'test', body:'test', creator_id:user.id)
+        post = user.created_posts.first
+        
+        expect(Like.create(liked_post_id:post.id, liking_user_id:user.id)).to be_truthy
     end
 
 end

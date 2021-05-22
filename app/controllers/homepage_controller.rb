097c40652f072c388187287effb504b5433be34c
Henry-Cantrell <hencan.1997@gmail.com>
2021-05-22 12:31:56 -0400

    Homepage controller updates

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -4,10 +4,17 @@ class HomepageController < ApplicationController
     end
 
     def create
+        @user = current_user
+        @new_post = @user.created_posts.build(post_params)
+
+        if @new_post.save
+            redirect_to root_path
+        end
     end
 
     private
 
     def post_params
+        params.require(post).permit(:body, :creator_id)
     end
 end

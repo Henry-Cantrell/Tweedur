db6cbc01f7d7519f9cc65630838b031e8e275cd0
Henry-Cantrell <hencan.1997@gmail.com>
2021-05-27 16:31:24 -0400

    User profile controller updated to recognize current user

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,5 +1,16 @@
 class UserProfilesController < ApplicationController
     def show
         @user = User.find(params[:id])
+
+        #Determine if user accessing profile is current user for 
+        #displaying the edit button
+
+        @current_user = false
+
+        if @user.id == current_user.id
+            @current_user = true
+        end
+
+        @user_posts = 
     end
 end

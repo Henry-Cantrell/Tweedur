5b4e642a3f3afa0b9414aade9a846285636cb206
Henry-Cantrell <hencan.1997@gmail.com>
2021-06-20 18:27:35 -0400

    Shows actual number of live notifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    before_action :notifications_setup
+
+    def notifications_setup
+        @follow_requests_length = current_user.following_users.where(accept_toggle: false).length
+    end
+
 end

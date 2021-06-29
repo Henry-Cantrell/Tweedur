6c550e9d8b9d6fc12160b30092ea7efd36372ccd
Henry-Cantrell <hencan.1997@gmail.com>
2021-06-28 23:07:33 -0400

    Seeing follower/followed posts updated to two way relationship in views/controller

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -2,7 +2,8 @@ class HomePagesController < ApplicationController
     
     def show
         @user = current_user
-        @follow_relationships = @user.followed_users.where(accept_toggle:true)
+        @followed_user_posts = @user.followed_users.where(accept_toggle:true)
+        @following_user_posts = @user.following_users.where(accept_toggle:true)
     end
 
 end

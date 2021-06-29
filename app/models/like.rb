db01e23177f618fec1f683f4e1075e385ba1186d
Henry-Cantrell <hencan.1997@gmail.com>
2021-06-28 22:22:45 -0400

    Ability to place likes added on controller/model level

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
     belongs_to :liking_user, class_name: 'User'
-    belongs_to :post_liked, class_name: 'Post'
+    belongs_to :liked_post, class_name: 'Post'
 
     validates :liking_user_id, presence: true
     validates :liked_post_id, presence: true

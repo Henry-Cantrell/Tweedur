8edcc3510aa6ac2a7310145dab86daef765d8ceb
Henry-Cantrell <hencan.1997@gmail.com>
2021-06-30 17:02:21 -0400

    Comments on posts now render to page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
 class CommentsController < ApplicationController
+
+def create
+	@new_comment = Comment.new(comment_params)
+
+	if @new_comment.save!
+		redirect_back(fallback_location: root_path)
+	end
+end
+
+def destroy
+	@trash_comment = Comment.find_by(comment_params)
+	
+	if @trash_comment.destroy!
+		redirect_back(fallback_location: root_path)
+	end
+end
+
+private
+
+	def comment_params
+		params.require(:comment).permit(:body, :author_id, :post_id)
+	end
+
 end

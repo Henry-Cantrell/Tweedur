942b9d6d11f121edf875478f95ccae457b52fbb6
Henry-Cantrell <hencan.1997@gmail.com>
2021-07-07 19:57:44 -0400

    Updated rspec tests for comment likes

--- spec/models/comment_like_spec.rb
+++ spec/models/comment_like_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe CommentLike, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can be created by a user on a comment" do
+        user = User.create(id:1, email:'test@test.com', password:'test123')
+        user.created_posts.create(id: 1, body:'test', creator_id:user.id)
+        post = user.created_posts.first
+
+	comment = Comment.create(post_id: post.id, author_id: user.id, body: 'test')
+        comment_like = CommentLike.create(id: 1, body: 'testing', comment_liker_id: user.id, comment_id: comment.id)
+
+        expect(comment_like).to be_truthy
+    end
 end

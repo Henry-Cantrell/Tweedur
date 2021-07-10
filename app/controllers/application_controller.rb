075e756eb2c79855fe2c4674dff60782948f2db3
Henry-Cantrell <hencan.1997@gmail.com>
2021-07-10 16:22:30 -0400

    Heroku deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     before_action :notifications_display_navbar
+    before_action { flash.clear }
 
     def notifications_display_navbar
         unless current_user.nil?
